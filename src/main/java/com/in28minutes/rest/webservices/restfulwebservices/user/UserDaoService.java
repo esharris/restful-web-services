abe4158bb34cee88caba4c1a2d44a81df3e0075e
Earl Harris <esharris@mac.com>
2022-12-29 16:47:59 -0500

    Lesson 20

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author earlharris
+ */
+@Component
+public class UserDaoService {
+    private static List<User> users = new ArrayList<>();
+    
+    static {
+        users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(2, "Eve", LocalDate.now().minusYears(25)));
+        users.add(new User(3, "Jim", LocalDate.now().minusYears(20)));
+    }
+    
+    public List<User> findAll() {
+        return users;
+    }
+}
