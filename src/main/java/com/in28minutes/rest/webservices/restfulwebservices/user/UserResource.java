84e996ee1b7b80b23d7692b8b4e2f2890f2f4eb5
Earl Harris <esharris@mac.com>
2022-12-31 13:15:38 -0500

    Lesson 23

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -7,6 +7,8 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -31,5 +33,10 @@ public class UserResource {
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
         return service.findOne(id);
+    }
+    
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user) {
+        service.save(user);
     }
 }
