12c6a9cb0494ffd05ed6f710610ab68e82200dd9
Earl Harris <esharris@mac.com>
2023-01-02 11:21:37 -0500

    Lesson 27

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/ErrorDetails.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.in28minutes.rest.webservices.restfulwebservices.exception;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author earlharris
+ */
+public class ErrorDetails {
+    
+    private LocalDateTime timeStamp;
+    private String message;
+    private String details;
+
+    public ErrorDetails(LocalDateTime timeStamp, String message, String details) {
+        this.timeStamp = timeStamp;
+        this.message = message;
+        this.details = details;
+    }
+
+    public LocalDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+    
+    
+}
